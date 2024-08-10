ab7abe0a23bada8b4d5651004545b745bad22cb9
kotabrog <suzuki.kota0331@gmail.com>
2024-08-10 11:13:51 +0900

    check cache

--- test_rust/src/main.rs
+++ test_rust/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let mut rng = rand::thread_rng();
     let x: f64 = rng.gen();
     let y = std::f64::consts::PI;
-    println!("Hello, world!! random num3: {}", x * y);
+    println!("Hello, world!! random num: {}", x * y);
 }
 
 #[cfg(test)]
